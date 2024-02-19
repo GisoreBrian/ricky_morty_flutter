3d2502fca34091a0eedb6e3c0614c993fa963035
GisoreBrian <freelancerkazungudev@gmail.com>
2024-02-19 13:21:28 +0300

    Flutter RickyMoertyApi app

---
+++ README.md
@@ -0,0 +1,6 @@
+# ricky_morty_flutter
+
+Dart wrapper over https://rickandmortyapi.com/
+
+## Screenshots
+
---
+++ android/app/src/main/kotlin/com/kazungudev/ricky_morty_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kazungudev.ricky_morty_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
